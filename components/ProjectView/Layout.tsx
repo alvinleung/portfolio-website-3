463da26621e5262ecf2b3c779e9044afce326811
Alvin Leung <alvinleung2009@gmail.com>
2022-09-13 16:07:26 -0700

    update content

--- components/ProjectView/Layout.tsx
+++ components/ProjectView/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactText, useMemo } from "react";
+import React from "react";
 
 type Props = {
   children: React.ReactNode;
@@ -8,7 +8,7 @@ type Props = {
 
 export const LayoutFull = ({ children, grid, extraMargin }: Props) => {
   const gridTemplateStringArr =
-    grid && grid.split("/").map((str) => str.trim());
+    grid && grid.split("/").map((str) => str.trim() + "fr");
 
   const gridTemplateString = gridTemplateStringArr
     ? gridTemplateStringArr.join(" ")
