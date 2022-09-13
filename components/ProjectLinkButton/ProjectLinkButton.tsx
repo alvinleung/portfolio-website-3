e5220e0e8f6e11598d65c96fd8b97e6a6a5afd79
Alvin Leung <alvinleung2009@gmail.com>
2022-09-12 17:54:39 -0700

    simplified project button approach

--- components/ProjectLinkButton/ProjectLinkButton.tsx
+++ components/ProjectLinkButton/ProjectLinkButton.tsx
@@ -15,7 +15,7 @@ type Props = {
   scrolled: boolean;
 };
 
-const ProjectLinkButton = ({ projectStyle, projectInfo }: Props) => {
+const ProjectLinkButton = ({ projectStyle, projectInfo, scrolled }: Props) => {
   const [isHovering, setIsHovering] = useState(false);
   const linkRef = useRef() as MutableRefObject<HTMLAnchorElement>;
   const { prevCardRef } = usePageTransition();
@@ -24,7 +24,21 @@ const ProjectLinkButton = ({ projectStyle, projectInfo }: Props) => {
     prevCardRef.current = linkRef.current;
   };
 
-  const color = projectStyle.dark;
+  // const baseColor = scrolled ? "rgba(50, 50, 50, 0.4)" : projectStyle.dark;
+  // const baseColorHover = scrolled
+  //   ? "rgba(50, 50, 50, 0.5)"
+  //   : projectStyle.accent;
+  // const textColor = scrolled ? "rgba(255, 255, 255, 1)" : projectStyle.accent;
+  // const textColorHover = scrolled ? textColor : projectStyle.dark;
+  // const circleColor = scrolled ? projectStyle.accent : projectStyle.accent;
+  // const circleColorHover = scrolled ? projectStyle.accent : projectStyle.dark;
+
+  const baseColor = projectStyle.dark;
+  const baseColorHover = projectStyle.accent;
+  const textColor = projectStyle.accent;
+  const textColorHover = projectStyle.dark;
+  const circleColor = projectStyle.accent;
+  const circleColorHover = projectStyle.dark;
 
   return (
     <Link href={getProjectLink(projectInfo.slug)}>
@@ -41,22 +55,22 @@ const ProjectLinkButton = ({ projectStyle, projectInfo }: Props) => {
           scale: 0.95,
         }}
         animate={{
-          backgroundColor: isHovering ? projectStyle.accent : color,
+          backgroundColor: isHovering ? baseColorHover : baseColor,
           // : "rgba(50,50,50,.6)",
-          color: isHovering ? color : projectStyle.accent,
+          color: isHovering ? textColorHover : textColor,
         }}
         className="inline-flex justify-center rounded-full leading-none py-3 px-4 cursor-pointer backdrop-blur-md"
       >
         <motion.div
           className="w-[1em] h-[1em] mr-3 rounded-full"
           animate={{
-            backgroundColor: isHovering ? color : projectStyle.accent,
+            backgroundColor: isHovering ? circleColorHover : circleColor,
           }}
           transition={{
             duration: AnimationConfig.FAST,
             ease: AnimationConfig.EASING,
           }}
-        ></motion.div>
+        />
         {projectInfo.title}
       </motion.a>
     </Link>
