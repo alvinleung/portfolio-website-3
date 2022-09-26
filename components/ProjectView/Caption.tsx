2bb01d24fbf4421be2fc4b9516e9edd9a84a489d
Alvin Leung <alvinleung2009@gmail.com>
2022-09-26 01:12:45 -0700

    update whatif content

--- components/ProjectView/Caption.tsx
+++ components/ProjectView/Caption.tsx
@@ -1,8 +1,11 @@
+import { useMemo } from "react";
+
 type CaptionProps = {
   label?: string;
   text: string;
   wideSpacing: boolean;
   forImage: boolean;
+  topPadding: boolean;
 };
 
 export const Caption = ({
@@ -10,18 +13,25 @@ export const Caption = ({
   text,
   wideSpacing,
   forImage = true,
+  topPadding = false,
 }: CaptionProps) => {
   const labelSpacing = wideSpacing
     ? "w-[14vw] lg:w-[7vw] 2xl:w-[14vw] opacity-50"
     : "w-[14vw] lg:w-[7vw] 2xl:w-[7vw] opacity-50";
 
+  const layout = useMemo(() => {
+    if (forImage) {
+      return "md:absolute md:mx-4 mt-3";
+    }
+    if (topPadding) {
+      return "md:col-start-1 row-start-1 md:row-start-auto mt-3";
+    }
+    return "md:col-start-1 row-start-1 md:row-start-auto";
+  }, [topPadding, forImage]);
+
   return (
     <figcaption
-      className={`${
-        forImage
-          ? "md:absolute md:mx-4 mt-3"
-          : "md:col-start-1 row-start-1 md:row-start-auto"
-      } flex flex-row gap-4 text-sm leading-5 top-0 mb-2`}
+      className={`${layout} flex flex-row gap-4 text-sm leading-5 top-0 mb-2`}
     >
       {/* one column around 7vw */}
       {label && <div className={labelSpacing}>{label}</div>}
