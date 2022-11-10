4ea25f5a0d0c71deed46e30bf4e161f6a8fb48e3
Alvin Leung <alvinleung2009@gmail.com>
2022-11-10 12:40:40 -0800

    Update typography

--- components/Layouts/LandingHero.tsx
+++ components/Layouts/LandingHero.tsx
@@ -85,7 +85,7 @@ const LandingHero = ({ isViewingGrid }: Props) => {
           }}
         >
           <div className="col-start-1 col-span-full 2xl:col-span-4 flex flex-col">
-            <h1 className="text-2xl sm:text-4xl lg:text-6xl font-normal tracking-[-.03em]">
+            <h1 className="text-2xl sm:text-4xl lg:text-6xl font-normal tracking-[-.016em] lg:leading-[1.01em]">
               Alvin Leung is an interaction designer. Informed by business and
               user needs, he thrives in using his aesthetic sensibility and
               prototyping wizardry to bring wild concepts from 0 to 1.
