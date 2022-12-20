a1b800578f1748ca487c8b45c2366f6b86205c2e
Alvin Leung <alvinleung2009@gmail.com>
2022-12-20 15:39:47 -0800

    fix cache

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { images: { allowFutureImage: true } },
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png|woff|woff2)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
