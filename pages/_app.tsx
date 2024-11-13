406bae609c6e4e1579cd2d215b3478a16ccb23af
alvin leung <alvinleung2009@gmail.com>
2024-11-13 14:11:33 -0800

    add stat counter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -54,6 +54,19 @@ function MyApp({ Component, pageProps, router }: AppProps) {
           </HomeScrollPositionContextProvider>
         </WindowDimensionContextProvider>
       </HistoryProvider>
+      {/* <!-- Default Statcounter code for portfolio https://alvinn.design --> */}
+      <script type="text/javascript">
+      var sc_project=12719687;
+      var sc_invisible=1;
+      var sc_security="ce1e7f94";
+      </script>
+      <script type="text/javascript"
+      src="https://www.statcounter.com/counter/counter.js" async></script>
+      <noscript><div class="statcounter"><a title="Web Analytics"
+      href="https://statcounter.com/" target="_blank"><img class="statcounter"
+      src="https://c.statcounter.com/12719687/0/ce1e7f94/1/" alt="Web Analytics"
+      referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+      {/* <!-- End of Statcounter Code --> */}
     </>
   );
 }
